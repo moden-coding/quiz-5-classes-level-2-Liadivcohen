e9491ec726db3b76e5e5495ebce7bfb72e64a8b9
Lia <29ldivcohen@gannacademy.org>
2025-12-09 10:03:00 -0500

    done

--- src/ArcadeGameCredit.java
+++ src/ArcadeGameCredit.java
@@ -1,3 +1,38 @@
 public class ArcadeGameCredit {
-    
+    private String gameName; // e.g. "Dance Machine"
+    private int credits; // how many credits are currently loaded
+    private int costPerPlay; // how many credits one play costs
+    private int playsCompleted; // used at higher levels
+
+    public ArcadeGameCredit(String gameName, int credits, int costPerPlay) {
+        this.gameName = gameName;
+         
+        if (credits < 0) {
+            credits = 0;
+        }
+        this.credits = credits;
+        if (costPerPlay <= 0) {
+            costPerPlay = 1;
+        }
+        this.costPerPlay = costPerPlay;
+        playsCompleted = 0;
+    }
+
+    public void playOnce() {
+        if (credits >= costPerPlay) {
+            credits -= costPerPlay;
+            playsCompleted++; //Increase playsCompleted by 1.
+        }
+        if (credits <= costPerPlay) {
+           // credits = credits;
+        }
+    }
+     public int getTotalCreditsUsed(){
+        return playsCompleted * costPerPlay;
+     }
+
+    public String toString() {
+        return "Game:" + gameName + " Credits: " + credits + " Cost Per Play: " + costPerPlay + " Plays completed: " + playsCompleted;
+    }
+
 }
